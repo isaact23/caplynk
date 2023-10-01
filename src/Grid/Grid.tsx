88336b454b49a900018ba4c27cf41678b9b50126
Isaac Thompson <shufflesninja@gmail.com>
2023-09-30 22:50:03 -0400

    Add SystemStatus component

--- src/Grid/Grid.tsx
+++ src/Grid/Grid.tsx
@@ -1,13 +1,11 @@
+import SystemStatus from "./SystemStatus/SystemStatux";
 
 function Grid() {
   return (
     <div className="w3-container">
       <div className="w3-row">
         <div className="w3-col s6 m4 l3">
-          <div className="w3-card-4 w3-margin w3-padding-large w3-center w3-round-large w3-brown">
-            <h3>System Status</h3>
-            
-          </div>
+          <SystemStatus />
         </div>
       </div>
     </div>
---
+++ src/Grid/SystemStatus/SystemStatux.tsx
@@ -0,0 +1,26 @@
+
+function SystemStatus() {
+  return (
+    <div className="w3-card-4 w3-margin w3-padding-large w3-round-large w3-brown">
+      <h3 className="w3-center">System Status</h3>
+      <div className="w3-bar">
+        <div className="w3-bar-item">
+          <img src="icons/x_icon.png" alt="status icon" style={{maxWidth: "30px"}}></img>
+        </div>
+        <div className="w3-bar-item">
+          <h4>esp32 servo</h4>
+        </div>
+      </div>
+      <div className="w3-bar">
+        <div className="w3-bar-item">
+          <img src="icons/check_icon.png" alt="status icon" style={{maxWidth: "30px"}}></img>
+        </div>
+        <div className="w3-bar-item">
+          <h4>Another system</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SystemStatus;
